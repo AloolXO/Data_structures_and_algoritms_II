390f03956209322e04a9ca1e9ac1277c8298a896
Artem Leunenko <al.university.edu@gmail.com>
2021-09-28 22:21:27 +1000

    Update 1.1

--- Ex2_Words/src/Ex1_Words.cpp
+++ Ex2_Words/src/Ex1_Words.cpp
@@ -1,5 +1,5 @@
 //============================================================================
-// Name        : Ex1_Words.cpp
+// Name        : Ex2_Words.cpp
 // Author      : AloolXO
 // Version     :
 // Copyright   : Your copyright notice
@@ -7,9 +7,56 @@
 //============================================================================
 
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <cstring>
+
 using namespace std;
 
-int main() {
-	cout << "Ex-Words" << endl; // prints Ex-Words
-	return 0;
+int countWords(string x) {
+    int Num = 0;
+    char prev = ' ';
+    for (unsigned int i = 0; i < x.size(); i++) {
+        if (x[i] != ' ' && prev == ' ') Num++;
+        prev = x[i];
+    }
+    return Num;
+}
+
+void countWord(string s, char c){
+	int k = 0;
+	for (int i = 1; i < s.length(); i++)
+		if (s[i] == c && s[i - 1] == ' ')
+			k++;
+	if (k)
+		cout << "Количество слов, начинающихся на букву " << c << ": " << k << endl;
+	else
+		cout << "Искомых слов не найдено!" << endl;
+	cout << "Процентное содержание слов на букву " << c << ": " << (double)k*100/countWords(s) << " %" << endl;
+}
+
+int main(){
+    setlocale(LC_ALL, "ru");
+    char y = 'у', p= 'п';
+    string s, word;
+    cout << "Анализируемый текст приведен в файле For_lesson_16092021.txt" << endl;
+    ifstream F;
+    F.open("C://University/2_course/3_semester/Data_structures_and_algoritms_II/Ex2_Words/input.txt");
+    if (F.is_open())
+    {
+        while (getline(F, s))
+        {
+            cout << s << endl;
+        }
+    }
+    F.close();
+    stringstream str(s);
+    cout << "Общее количество слов: " << countWords(s) << endl;
+
+    countWord(s, y);
+    countWord(s, p);
+
+    cin.get();
+    system("pause");
 }
