4a8fcd4ca1392cdcd0772516840a0e6da0ff9ee9
Artem Leunenko <al.university.edu@gmail.com>
2021-09-28 21:06:18 +1000

    Ex2_words
    Lecrute and practical 1, ex 2:

---
+++ Ex2_Words/src/Ex1_Words.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Ex1_Words.cpp
+// Author      : AloolXO
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Ex-Words" << endl; // prints Ex-Words
+	return 0;
+}
