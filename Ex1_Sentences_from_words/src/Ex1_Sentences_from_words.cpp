968bcb0ecb939630aaae32c8971cd8c51b5f99dd
Artem Leunenko <al.university.edu@gmail.com>
2021-09-28 22:45:47 +1000

    Ex1_Sentences_from_words
    https://docs.google.com/document/d/1VGIqYSDx0xG4L9pcSRCPJI1trpe4jA24/edit?usp=sharing&ouid=109589191153794027457&rtpof=true&sd=true

---
+++ Ex1_Sentences_from_words/src/Ex1_Sentences_from_words.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Ex1_Sentences_from_words.cpp
+// Author      : AloolXO
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Sentences from words" << endl; // prints Sentences from words
+	return 0;
+}
